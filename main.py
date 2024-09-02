dd2e6de89fc6462e5e690baad977dde7e3e6b84a
Youssef-Bassem <youssef-bassem90@outlook.com>
2024-09-02 22:08:22 +0300

    Initial Version

---
+++ main.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def division(a, b):
+    return a / b
+
+def multiplication(a, b):
+    return a * b
+
+
+print(add(6,2))
+
+print(subtract(6,2))
+
+print(division(6,2))
+
+print(multiplication(6,2))
+
---
+++ unittest.py
@@ -0,0 +1,20 @@
+import unittest
+import main as main
+
+class TestDummy(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(main.add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(main.subtract(5, 3), 2)
+
+    def test_division(self):
+        self.assertEqual(main.division(6, 2), 3)
+
+    def test_multiplication(self):
+        self.assertEqual(main.multiplication(6, 2), 12)
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
