5ef5c01987efab08c2f64c88ef0646fbf46b6e1b
Youssef-Bassem <youssef-bassem90@outlook.com>
2024-09-02 22:15:16 +0300

    commit

--- main.py
+++ main.py
@@ -7,15 +7,5 @@ def subtract(a, b):
 def division(a, b):
     return a / b
 
-def multiplication(c, d):
-    return c * d
-
-
-print(add(6,2))
-
-print(subtract(6,2))
-
-print(division(6,2))
-
-print(multiplication(6,2))
-
+def multiplication(a, b):
+    return a * b
